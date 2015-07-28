a92e8fc8801fe88b60035b49969129d8e7f761d0
Brian Eccles <brian.e@bnrobotics.com>
2015-07-28 11:27:37 -0400

    Removed emacs configuration

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s -b $ROOT_DIR/.emacs ~/.emacs
 ln -s -b $ROOT_DIR/.bashrc ~/.bashrc
 ln -s -b $ROOT_DIR/.gitconfig ~/.gitconfig
 ln -s -b $ROOT_DIR/.dircolors ~/.dircolors
