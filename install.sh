0bf9f3f0bc33b69b2887d54eaa2b1f1a3ead3c0b
Brian Eccles <brian.e@bnrobotics.com>
2015-07-28 15:43:29 -0400

    Removed Z shell and tmux configuration

--- install.sh
+++ install.sh
@@ -5,7 +5,5 @@ ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ln -s -b $ROOT_DIR/.bashrc ~/.bashrc
 ln -s -b $ROOT_DIR/.gitconfig ~/.gitconfig
 ln -s -b $ROOT_DIR/.dircolors ~/.dircolors
-ln -s -b $ROOT_DIR/.tmux.conf ~/.tmux.conf
-ln -s -b $ROOT_DIR/.zshrc ~/.zshrc
 ln -s -b $ROOT_DIR/.ssh_config ~/.ssh/config
 
